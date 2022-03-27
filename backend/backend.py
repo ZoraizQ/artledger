216848c56eef15828eb54c64dbf065a3c931e67b
Ethan Kacena-Merrell <gen7hz@virginia.edu>
2022-03-26 22:39:54 -0400

    flask server added

--- backend/backend.py
+++ backend/backend.py
@@ -6,7 +6,10 @@ from Posts import Posts
 from Trade import Trade
 from Deso import Deso
 import pprint
+from flask import Flask
+from flask import request
 import pyrebase
+
 config = {
   "apiKey": "AIzaSyBC8sBWUtc7fvPbPJ_3a2p51IH20RLJ-8Y",
   "authDomain": "desoart-172d0.firebaseapp.com",
@@ -17,13 +20,19 @@ config = {
 }
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
+
+@app.route('/sign_up',methods=['POST'])
 def sign_up(public_key,type):
+    public_key = request.form["public_key"]
+    type = request.form["type"]
     username = Users.getUsernameFromKey(public_key)
     data = {
-        "username":username
+        "username":username,
+        "liked": list()
     }
     results = db.child(type).push(data,public_key) #type can only be consumers or artists
 
+@app.route('/get_random_posts')
 def get_random_posts():
     artists = list(db.child("artists").get().values())
     chosen = list()
@@ -39,7 +48,14 @@ def get_random_posts():
 
     return chosen
 
+@app.route('/get_consumer_info/<public_key>')
+def get_consumer_info(public_key):
+    info = {}
+    info["username"] = Users.getUsernameFromKey(public_key)
+    info["profile_pic"] = Users.getProfilePic(public_key)
+    return info
 
+@app.route('/get_consumer_info/<public_key>')
 def get_artists_info(public_key):
     info = {}
     info["username"] = Users.getUsernameFromKey(public_key)
@@ -52,26 +68,40 @@ def get_artists_info(public_key):
     info["likes"] = total
     return info
 
-
+@app.route('/get_posts/<public_key>')
 def get_posts(public_key):
     return Posts.getUserPosts(public_key)
 
+@app.route('/get_single_posts/<postHashHex>')
+def get_single_post(postHashHex):
+    return Posts.getPostInfo(postHashHex)
 
 
+#like function
+@app.route('/like_post',methods=['POST'])
+def like_post(postHashHex,consumer_public_key):
+    public_key = request.form["public_key"]
+    postHashHex = request.form["postHasHex"]
+    Post.like(postHashHex)
+    db.child("consumers").update({"liked": list.append(postHashHex) },consumer_public_key)
 
-pp = pprint.PrettyPrinter(width=41, compact=True)
-id = "BC1YLiyxrKs33n3mwPDaT2h7rNigZNSxTAAe1SY9SVkoeQA2h5A7b3p"
-seed = "a1c2941bf195fb6d6d4771ccb0d95d8adf1099b83de95ec6ac22c990bc528051"
+#get consumers liked posts
+@app.route('/get_liked_posts/<public_key>')
+def get_liked_posts(public_key):
+    posthash = db.child("consumers").get("liked",public_key)
+    output = list()
+    for i in posthash:
+        output.append(get_single_post(i))
+    return output
+
+def upload_image(seed, public_key,path, contentin):
+    post = Post(seedHex=seed,publicKey=public_key)
+    imageFileList=[
+    ('file',('screenshot.jpg',open(path, "rb"),'image/png'))
+    ]
+    url = post.uploadImage(imageFileList)
+    post.send(content=contentin,imageUrl= url['ImageURL'])
 
 
-deso = Deso(id)
-post = Post(seedHex=seed,publicKey=id)
-imageFileList=[
-  ('file',('screenshot.jpg',open("answer 4.png", "rb"),'image/png'))
-]
-url = post.uploadImage(imageFileList)
-post.send(content="This is my first post",imageUrl= url['ImageURL'])
+app.run(debug=True)
 
-#listOfPublicKeys = [id] # you can pass multiple public key of users
-#pp.pprint(Users.getUserStateless(listOfPublicKeys))
-#pp.pprint(deso.getDeSoPrice())
