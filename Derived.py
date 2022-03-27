0553c60d0af68c16cd19508976c252b4648efb60
Ethan Kacena-Merrell <gen7hz@virginia.edu>
2022-03-26 21:45:42 -0400

    add: python deso integration and pyrebase

---
+++ Derived.py
@@ -0,0 +1,26 @@
+from Route import getRoute
+import requests
+from base58 import b58decode_check
+
+
+def addExtraData(transactionHex, derivedKey):
+    compressed_key = b58decode_check(derivedKey)[3:].hex()
+    payload = {"TransactionHex": transactionHex,
+               "ExtraData": {"DerivedPublicKey": compressed_key}}
+    endpoint = getRoute() + "append-extra-data"
+    res = requests.post(endpoint, json=payload)
+    TransactionHex = res.json()["TransactionHex"]
+    return TransactionHex
+
+
+def addExtraDataDict(transactionHex, extraData):
+    payload = {"TransactionHex": transactionHex,
+               "ExtraData": extraData}
+    endpoint = getRoute() + "append-extra-data"
+    res = requests.post(endpoint, json=payload)
+    try:
+        TransactionHex = res.json()["TransactionHex"]
+    except:
+        print(res.json())
+    return TransactionHex
+
