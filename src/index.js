c2f3d8ee3a1afb4cf2ac164e563d7b86afaa9861
ZoraizQ <zoraizq@outlook.com>
2022-03-29 01:12:12 -0400

    sw, manifest

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +10,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+registerServiceWorker();
